2f7c6010eca1af128041e546f9d3e799e06967c7
Keyur Desai <54926128+KeyurDesai53987@users.noreply.github.com>
2022-12-31 13:29:12 +0530

    Update index.py

--- index.py
+++ index.py
@@ -10,17 +10,31 @@ st.write('Download path: ', down_path)
 # ------ CODE ------
 
 if st.button("Download"):
-    val = d.check_link(url)
-    #down_path = d.file_path()
+    
+    if 'DESKTOP_SESSION' not in os.environ: #and os.environ('HOSTNAME')=='streamlit':
+        '''
+        with open(title, 'rb') as f:
+            bytes = f.read()
+            b64 = base64.b64encode(bytes).decode()
+            href = f'<a href="data:file/zip;base64,{b64}" download=\'{title}\'>\
+                Here is your link \
+            </a>'
+            st.markdown(href, unsafe_allow_html=True)
 
-    if val == 'plist':
-        res = d.playlist_down(url)
-    elif val == 'single':
-        res = d.downloading(url)
+        os.remove(title)'''
+        st.error('Can not download online')
     else:
-        res = 'no'
+        val = d.check_link(url)
+        #down_path = d.file_path()
 
-    if res == 'no':
-        st.error(res)
-    else:
-        st.success(res)
+        if val == 'plist':
+            res = d.playlist_down(url)
+        elif val == 'single':
+            res = d.downloading(url)
+        else:
+            res = 'no'
+
+        if res == 'no':
+            st.error(res)
+        else:
+            st.success(res)
