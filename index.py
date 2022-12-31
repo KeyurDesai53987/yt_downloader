9bdf3d828434b4d4305742a516fd5930874f049b
Keyur Desai <54926128+KeyurDesai53987@users.noreply.github.com>
2022-12-31 13:43:45 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ import shutil
 url = st.text_input("YT Url: ")
 down_path = d.file_path()
 total, used, free = shutil.disk_usage(down_path)
-st.write('Download path: {}\nSize: {}'.format(down_path, stat))
+st.write('Download path: {}'.format(down_path))
 st.write("Total: %d GiB" % (total // (2**30)))
 st.write("Used: %d GiB" % (used // (2**30)))
 st.write("Free: %d GiB" % (free // (2**30)))
