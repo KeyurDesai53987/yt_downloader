1361090ca804a42ce3df481757e94d472fe88aec
Keyur Desai <54926128+KeyurDesai53987@users.noreply.github.com>
2023-01-02 12:05:42 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pyautogui
 #from main import start
 import code as d
 import shutil
@@ -28,38 +29,38 @@ def down_url(url):
     else:
         st.success(res)
 
-def start():
-    # streamlit assets 
-    number = None
-    option = None
-    url = None
-    url = st.text_input("YT Url: ")
-    down_path = d.file_path()
-    try:
-        total, used, free = shutil.disk_usage(down_path)
-        files = os.listdir(down_path)
-        st.write('Download path: {}'.format(down_path))
-        st.write("Total: %d GiB" % (total // (2**30)))
-        st.write("Used: %d GiB" % (used // (2**30)))
-        st.write("Free: %d GiB" % (free // (2**30)))
-        st.write("Files: ")
-        #st.write(files)
-        print(files)
-    except:
-        st.write('Can\'t find download path')
-        st.write('Download Something')
+#def start():
+# streamlit assets 
+number = None
+option = None
+url = None
+url = st.text_input("YT Url: ")
+down_path = d.file_path()
+try:
+    total, used, free = shutil.disk_usage(down_path)
+    files = os.listdir(down_path)
+    st.write('Download path: {}'.format(down_path))
+    st.write("Total: %d GiB" % (total // (2**30)))
+    st.write("Used: %d GiB" % (used // (2**30)))
+    st.write("Free: %d GiB" % (free // (2**30)))
+    st.write("Files: ")
+    #st.write(files)
+    print(files)
+except:
+    st.write('Can\'t find download path')
+    st.write('Download Something')
 
-    try:
-        option = st.selectbox(
-            'What would you like to download?',
-            files)
-        st.write('You selected:', option)
-    except:
-        st.write("Files not present in directory")
+try:
+    option = st.selectbox(
+        'What would you like to download?',
+        files)
+    st.write('You selected:', option)
+except:
+    st.write("Files not present in directory")
 
 # ------ CODE ------
 
-begin = start()        
+#begin = start()        
 
 if st.button("Download"):
     if option==None:
@@ -73,11 +74,13 @@ if st.button("Download"):
         st.write(p)
         st.markdown(get_binary_file_downloader_html(p, 'Video'), unsafe_allow_html=True)
         st.write('{} is downloaded'.format(option))
-     
+          
 if st.button("Refresh"):
-    again = start()
-        
+    pyautogui.hotkey("ctrl","F5")            
+            
 def dummy():
+    if st.button("Refresh"):
+        again = start()
     if val == 'Invalid Link!!!':
         p = down_path + '{}'.format('\\') + files[number]
         st.write(p)
